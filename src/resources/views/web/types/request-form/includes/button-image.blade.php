f8b977cc661cd295f337412a81c60a1129d7b18b
Labrock <vladimirpeshkov5@yandex.ru>
2026-03-06 13:00:51 +0300

    change class

--- src/resources/views/web/types/request-form/includes/button-image.blade.php
+++ src/resources/views/web/types/request-form/includes/button-image.blade.php
@@ -1,6 +1,6 @@
 @php($fileName = $item->recordable->image->file_name)
 <a href="{{ route('thumb-img', ['template' => 'original', 'filename' => $fileName]) }}"
-   data-fslightbox="lightbox-{{ $item->id }}" class="{{ $isFullPage ? 'sticky top-0' : 'inline-block mb-indent-half' }}">
+   data-fslightbox="lightbox-{{ $item->id }}" class="{{ $isFullPage ? 'sticky top-sticky' : 'inline-block mb-indent-half' }}">
     <picture class="not-prose">
         <source media="(min-width: 1024px)" srcset="{{ route('thumb-img', ['template' => $isFullPage ? 'image-text-record' : 'request-form-record-two-thirds', 'filename' => $fileName]) }}">
         <source media="(min-width: 480px)" srcset="{{ route('thumb-img', ['template' => 'image-text-record-tablet', 'filename' => $fileName]) }}">
